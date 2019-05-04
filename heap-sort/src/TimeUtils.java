2fc5a06de6f84dd77b59599396295e9c9512e9e8
gabriel passos <gabriel.passos@agibank.com.br>
2019-05-04 14:53:53 -0300

    :recycle: ajuste no time utils

--- heap-sort/src/TimeUtils.java
+++ heap-sort/src/TimeUtils.java
@@ -4,7 +4,7 @@ import java.time.temporal.ChronoUnit;
 
 public class TimeUtils {
 
-    public static ZoneId BR_ZONE = ZoneId.of("America/Sao_Paulo");
+    private static ZoneId BR_ZONE = ZoneId.of("America/Sao_Paulo");
 
     public static LocalDateTime now() {
         return LocalDateTime.now(BR_ZONE);
