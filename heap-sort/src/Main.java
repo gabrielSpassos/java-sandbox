bb5e2ee3fae7586ba8df7bb1fc7d48698fb3da26
gabriel passos <gabriel.passos@agibank.com.br>
2019-05-04 15:10:16 -0300

    :recycle: ajuste na main

--- heap-sort/src/Main.java
+++ heap-sort/src/Main.java
@@ -5,19 +5,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Realizando os testes com heap sort");
-        System.out.println("Melhor Caso");
         HeapSort heapSort = new HeapSort();
+        System.out.println("Pior Caso");
         LocalDateTime start = TimeUtils.now();
-        int[] sortedArray = heapSort.sort(getAscArray());
+        int[] sortedArray = heapSort.sort(getDescArray());
         LocalDateTime finish = TimeUtils.now();
         System.out.println("Inicio: " + start);
         System.out.println("Fim: " + finish);
         System.out.println("Tempo demorado em ms: " + TimeUtils.getDifferenceBetweenDates(start, finish));
         System.out.println("Array ordenado " + Arrays.toString(sortedArray));
         System.out.println("*******************************************\n\n");
-        System.out.println("Pior Caso");
+        System.out.println("Melhor Caso");
         start = TimeUtils.now();
-        sortedArray = heapSort.sort(getDescArray());
+        sortedArray = heapSort.sort(getAscArray());
         finish = TimeUtils.now();
         System.out.println("Inicio: " + start);
         System.out.println("Fim: " + finish);
