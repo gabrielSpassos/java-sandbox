c342772217bdabe2e346ef5f40cd558d9e670630
GABRIEL SANTOS DOS PASSOS <ALU201514565@uniritter.local>
2019-09-10 19:52:31 -0300

    :recycle: ajuste no analisador

--- compiler/src/main/java/com/gabrielspassos/poc/AnalisadorLexico.java
+++ compiler/src/main/java/com/gabrielspassos/poc/AnalisadorLexico.java
@@ -18,7 +18,7 @@ public class AnalisadorLexico {
         while((intch = pushbackReader.read()) != -1) {
             char character = (char) intch;
             if(character != ' ' && character != '\n') {
-                Token token = getToken(character);
+                Token token = getToken(character, pushbackReader);
                 if (token != null) {
                     tokens.add(token);
                 }
@@ -32,25 +32,35 @@ public class AnalisadorLexico {
         return new PushbackReader(new BufferedReader(new InputStreamReader(new FileInputStream(codeFileName), ENCODING)));
     }
 
-    private Token getToken(char character) {
+    private Token getToken(char character, PushbackReader pushbackReader) throws IOException {
         if(Character.isLetter(character)) {
-            return handleIdentifierAndReservedWord(character);
+            return handleIdentifierAndReservedWord(character, pushbackReader);
         }
 
         if(Character.isDigit(character)) {
-            return handleDigit(character);
+            return handleDigit(character, pushbackReader);
         }
 
         return null;
     }
 
-    private Token handleDigit(char character) {
+    private Token handleDigit(char character, PushbackReader pushbackReader) throws IOException {
         String num = String.valueOf(character);
+        char nextCharacter = (char) pushbackReader.read();
+        while (Character.isDigit(nextCharacter)) {
+            num = num.concat(String.valueOf(nextCharacter));
+            nextCharacter = (char) pushbackReader.read();
+        }
         return new Token(Tipo.SNUMERO, num);
     }
 
-    private Token handleIdentifierAndReservedWord(char character) {
+    private Token handleIdentifierAndReservedWord(char character, PushbackReader pushbackReader) throws IOException {
         String id = String.valueOf(character);
+        char nextCharacter = (char) pushbackReader.read();
+        while (Character.isLetter(nextCharacter)) {
+            id = id.concat(String.valueOf(nextCharacter));
+            nextCharacter = (char) pushbackReader.read();
+        }
         Tipo tipo = Tipo.getTipoById(id);
         return new Token(tipo, id);
     }
