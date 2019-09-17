6409cf9c542626c7d185298fc3bda6b6a119c45b
GABRIEL SANTOS DOS PASSOS <ALU201514565@uniritter.local>
2019-09-17 20:26:38 -0300

    :recycle: ajuste para linha e coluna

--- compiler/src/main/java/com/gabrielspassos/poc/AnalisadorLexico.java
+++ compiler/src/main/java/com/gabrielspassos/poc/AnalisadorLexico.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class AnalisadorLexico {
 
     private static final String ENCODING = "US-ASCII";
+    private int linha = 1;
+    private int coluna = 1;
 
     private static final List<String> SPECIAL_CHARS = Arrays.asList(".", ":", ";", "(", ")");
     private static final String DOIS_PONTOS = ":";
@@ -15,25 +17,50 @@ public class AnalisadorLexico {
     public List<Token> analisar(String codeFileName) throws IOException {
         PushbackReader pushbackReader = getPushbackReader(codeFileName);
         List<Token> tokens = new ArrayList<>();
-        int intch;
-        int linha = 1;
-        int coluna = 1;
 
-        while((intch = pushbackReader.read()) != -1) {
-            char character = (char) intch;
-            if(character != ' ' && character != '\n') {
-                Token token = getToken(character, pushbackReader);
-                if (token != null) {
-                    tokens.add(token);
-                }
+        char character;
+        do {
+            character = readChar(pushbackReader);
+            if (character == ' ' || character == '\n' || character == '@') {
+                continue;
             }
-        }
+            Token token = getToken(character, pushbackReader);
+            if (token != null) {
+                tokens.add(token);
+            }
+        } while (character != '@');
 
         return tokens;
     }
 
     private PushbackReader getPushbackReader(String codeFileName) throws FileNotFoundException, UnsupportedEncodingException {
         return new PushbackReader(new BufferedReader(new InputStreamReader(new FileInputStream(codeFileName), ENCODING)));
+    }
+
+    private char readChar(PushbackReader pushbackReader) throws IOException {
+        int intch = pushbackReader.read();
+        char character = (char) intch;
+        if(character == '\n') {
+            linha++;
+            coluna = 1;
+        } else {
+            coluna++;
+        }
+
+        if(intch != -1) {
+            return character;
+        } else {
+            return '@';
+        }
+    }
+
+    private void unreadChar(PushbackReader pushbackReader, char character) throws IOException {
+        pushbackReader.unread(character);
+        if(character == '\n') {
+            linha--;
+        } else {
+            coluna--;
+        }
     }
 
     private Token getToken(char character, PushbackReader pushbackReader) throws IOException {
@@ -59,22 +86,22 @@ public class AnalisadorLexico {
 
     private Token handleIdentifierAndReservedWord(char character, PushbackReader pushbackReader) throws IOException {
         String id = String.valueOf(character);
-        char nextCharacter = (char) pushbackReader.read();
+        char nextCharacter = readChar(pushbackReader);
         while (Character.isLetter(nextCharacter)) {
             id = id.concat(String.valueOf(nextCharacter));
-            nextCharacter = (char) pushbackReader.read();
+            nextCharacter = readChar(pushbackReader);
         }
-        pushbackReader.unread((int) nextCharacter);
+        unreadChar(pushbackReader, nextCharacter);
         Tipo tipo = Tipo.getTipoById(id);
-        return new Token(tipo, id);
+        return new Token(tipo, id, linha, coluna);
     }
 
     private Token handlePlusAndMinusOperation(char character, PushbackReader pushbackReader) throws IOException {
         String operator = String.valueOf(character);
-        char nextCharacter = (char) pushbackReader.read();
+        char nextCharacter = readChar(pushbackReader);
         if (Character.isSpaceChar(nextCharacter)) {
             Tipo tipo = Tipo.getTipoById(operator);
-            return new Token(tipo, operator);
+            return new Token(tipo, operator, linha, coluna);
         }
         Token token = handleDigit(nextCharacter, pushbackReader);
         token.setLexema(operator.concat(token.getLexema()));
@@ -84,26 +111,26 @@ public class AnalisadorLexico {
 
     private Token handleDigit(char character, PushbackReader pushbackReader) throws IOException {
         String num = String.valueOf(character);
-        char nextCharacter = (char) pushbackReader.read();
+        char nextCharacter = readChar(pushbackReader);
         while (Character.isDigit(nextCharacter) || Tipo.SPONTO.getId().equals(String.valueOf(nextCharacter))) {
             num = num.concat(String.valueOf(nextCharacter));
-            nextCharacter = (char) pushbackReader.read();
+            nextCharacter = readChar(pushbackReader);
         }
-        pushbackReader.unread((int) nextCharacter);
-        return new Token(Tipo.SNUMERO, num);
+        unreadChar(pushbackReader, nextCharacter);
+        return new Token(Tipo.SNUMERO, num, linha, coluna);
     }
 
     private Token handleSpecialChars(char character, PushbackReader pushbackReader) throws IOException {
         String specialCharacter = String.valueOf(character);
         if (DOIS_PONTOS.equals(specialCharacter)) {
-            char nextCharacter = (char) pushbackReader.read();
+            char nextCharacter = readChar(pushbackReader);
             specialCharacter = specialCharacter.concat(String.valueOf(nextCharacter));
             Tipo tipoById = Tipo.getTipoById(specialCharacter);
             if (!Tipo.SATRIBUICAO.equals(tipoById)) {
-                pushbackReader.unread(nextCharacter);
+                unreadChar(pushbackReader, nextCharacter);
             }
         }
         Tipo tipo = Tipo.getTipoById(specialCharacter);
-        return new Token(tipo, specialCharacter);
+        return new Token(tipo, specialCharacter, linha, coluna);
     }
 }
