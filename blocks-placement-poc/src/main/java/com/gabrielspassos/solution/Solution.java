0644ac2e828f6596a5527323012a7b59913a761b
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-01-14 05:39:06 -0800

    feat: update block placement poc

--- blocks-placement-poc/src/main/java/com/gabrielspassos/solution/Solution.java
+++ blocks-placement-poc/src/main/java/com/gabrielspassos/solution/Solution.java
@@ -6,14 +6,41 @@ import java.util.stream.Stream;
 
 public class Solution {
 
+    private static final int START = 0;
+
     public List<Boolean> getResults(int[][] queries) {
         var results = new ArrayList<Boolean>();
+        var obstacles = new ArrayList<Integer>();
         for (int[] query : queries) {
             var queryType = getQueryType(query);
-            System.out.println(queryType);
+            System.out.println("Query type: " + queryType);
+
+            if (QueryType.ONE == queryType) {
+                obstacles.add(query[1]);
+                obstacles.sort(Integer::compareTo);
+                System.out.println("Obstacles: " + obstacles);
+            } else {
+                var maxLimit = query[1];
+                var size = query[2];
+                System.out.println("Max limit: " + maxLimit);
+                System.out.println("Size: " + size);
+
+                var isPossible = isPossibleToPlaceBlock(obstacles, maxLimit, size);
+                results.add(isPossible);
+            }
         }
 
         return results;
+    }
+
+    private Boolean isPossibleToPlaceBlock(ArrayList<Integer> obstacles, int maxLimit, int size) {
+        var distances = new ArrayList<Integer>();
+        for (int i = 0; i < obstacles.size(); i++) {
+            var distance = START == i ? obstacles.get(i) - START : obstacles.get(i) - obstacles.get(i - 1);
+            distances.add(distance);
+        }
+
+        return distances.stream().anyMatch(distance -> size <= distance);
     }
 
     private QueryType getQueryType(int[] query) {
