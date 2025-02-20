b5253376359d040c3958cd39cae7601ce13cdf5a
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-02-20 05:00:14 -0800

    feat: remove poc

--- pojos-poc/src/main/java/com/gabrielspassos/Contract.java
+++
@@ -1,9 +0,0 @@
-package com.gabrielspassos;
-
-import com.gabrielspassos.innter.ConnectCreditCardRequest;
-import com.gabrielspassos.innter.UpdateCreditCardResponse;
-
-public interface Contract {
-
-    UpdateCreditCardResponse update(ConnectCreditCardRequest request);
-}
