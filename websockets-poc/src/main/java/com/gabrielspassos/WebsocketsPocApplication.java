a2cc9188d8ef5d851d3ef717ace186fd6a10105f
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-01-06 09:01:35 -0300

    feat: add websockets poc

---
+++ websockets-poc/src/main/java/com/gabrielspassos/WebsocketsPocApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielspassos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketsPocApplication.class, args);
+	}
+
+}
