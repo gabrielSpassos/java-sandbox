435a489297dff5eacca179c5982f53bf7d7c3e28
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-02-12 08:31:40 -0300

    refactor bit map

--- bitmap-poc/README.md
+++ bitmap-poc/README.md
@@ -1,5 +1,13 @@
 # BitMap POC
 
+## What is?
+
+A bitmap is a compact, indexable data structure that represents a set of boolean 
+states using individual bits stored inside machine words, enabling constant-time 
+bitwise operations and high memory efficiency.
+
+Instead of storing [true, false, true, false] store like 1010
+
 ## Outputs
 
 ### Tests
--- bitmap-poc/src/main/java/com/gabrielspassos/Main.java
+++ bitmap-poc/src/main/java/com/gabrielspassos/Main.java
@@ -7,13 +7,19 @@ public class Main {
         BitMap bm = new BitMap(128);
 
         bm.set(3);
+        bm.set(7);
         bm.set(64);
         bm.toggle(3);
 
         System.out.println(bm.get(3));
+        System.out.println(bm.get(7));
         System.out.println(bm.get(64));
+        System.out.println(bm.get(70));
         System.out.println(bm.cardinality());
 
         bm.forEachSetBit(System.out::println);
+
+        bm.clear(7);
+        System.out.println(bm.get(7));
     }
 }
