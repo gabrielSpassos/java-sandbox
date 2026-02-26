89e3cded3ac197dbfd9d0df535245a8607e25c94
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-02-26 08:30:39 -0300

    feat: add decorator tests

---
+++ design-patterns-poc/src/test/java/com/gabrielspassos/patterns/structural/decorator/DecoratorTest.java
@@ -0,0 +1,29 @@
+package com.gabrielspassos.patterns.structural.decorator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DecoratorTest {
+
+    @Test
+    void shouldCreateSportsCar() {
+        Car sportsCar = new SportsCar(new BasicCar());
+
+        assertEquals("Sports Car | Basic Car", sportsCar.assemble());
+    }
+
+    @Test
+    void shouldCreateLuxuryCar() {
+        Car luxuryCar = new LuxuryCar(new BasicCar());
+
+        assertEquals("Luxury Car | Basic Car", luxuryCar.assemble());
+    }
+
+    @Test
+    void shouldCreateLuxurySportCar() {
+        Car luxuryCar = new LuxuryCar(new SportsCar(new BasicCar()));
+
+        assertEquals("Luxury Car | Sports Car | Basic Car", luxuryCar.assemble());
+    }
+}
