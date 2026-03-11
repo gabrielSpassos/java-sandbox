e95137bbe27c2aceb07e1fddb796427c26ef14e7
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-03-11 09:09:42 -0300

    feat: update double linked list

--- double-linked-list-poc/src/main/java/com/gabrielspassos/Main.java
+++ double-linked-list-poc/src/main/java/com/gabrielspassos/Main.java
@@ -3,5 +3,29 @@ package com.gabrielspassos;
 public class Main {
     static void main() {
         IO.println("Double Linked List POC!");
+
+        DoubleLinkedList<Integer> doubleLinkedList = new DoubleLinkedList<>();
+
+        doubleLinkedList.insertAtBeginning(1);
+        doubleLinkedList.insertAtBeginning(2);
+        doubleLinkedList.insertAtBeginning(3);
+
+        IO.println("Forward: " + doubleLinkedList.traverseForward());
+        IO.println("Backward: " + doubleLinkedList.traverseBackward());
+
+        doubleLinkedList.delete(3);
+        doubleLinkedList.delete(4);
+        doubleLinkedList.delete(1);
+        doubleLinkedList.delete(2);
+
+        IO.println("Empty Forward: " + doubleLinkedList.traverseForward());
+        IO.println("Empty Backward: " + doubleLinkedList.traverseBackward());
+
+        doubleLinkedList.insertAtEnd(1);
+        doubleLinkedList.insertAtEnd(2);
+        doubleLinkedList.insertAtEnd(3);
+
+        IO.println("Forward: " + doubleLinkedList.traverseForward());
+        IO.println("Backward: " + doubleLinkedList.traverseBackward());
     }
 }
