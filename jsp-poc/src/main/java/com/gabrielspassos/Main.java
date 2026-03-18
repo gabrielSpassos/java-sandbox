785fe0010921c96632677086ca4ad1a1a7c4493f
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-03-18 08:57:23 -0300

    feat: update jsp poc

--- jsp-poc/src/main/java/com/gabrielspassos/Main.java
+++ jsp-poc/src/main/java/com/gabrielspassos/Main.java
@@ -1,7 +1,22 @@
 package com.gabrielspassos;
 
+import org.eclipse.jetty.ee10.webapp.WebAppContext;
+import org.eclipse.jetty.server.Server;
+
 public class Main {
-    static void main() {
+
+    static void main() throws Exception {
         IO.println("JSP POC!");
+        var server = new Server(8080);
+
+        var context = new WebAppContext();
+        context.setContextPath("/");
+        context.setBaseResourceAsString("src/main/webapp");
+
+        server.setHandler(context);
+
+        server.start();
+        server.join();
     }
+    
 }
