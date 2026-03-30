dcae5841a737d85ea873cb13cb92fdb847600782
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-03-30 09:23:32 -0300

    test: update tests

--- segment-tree-poc/src/test/java/com/gabrielspassos/SegmentTreeTest.java
+++ segment-tree-poc/src/test/java/com/gabrielspassos/SegmentTreeTest.java
@@ -23,4 +23,33 @@ class SegmentTreeTest {
         assertEquals(expected, tree);
     }
 
+    @Test
+    void shouldUpdateTree() {
+        long[] input = {10, 5, 15, 3, 7};
+        List<Long> expected = List.of(0L, 61L, 41L, 20L, 31L, 10L, 5L, 15L, 24L, 7L);
+
+        SegmentTree segmentTree = new SegmentTree(input);
+
+        assertNotNull(segmentTree);
+        assertNotNull(segmentTree.getTree());
+
+        segmentTree.update(3, 24);
+
+        List<Long> tree = Arrays.stream(segmentTree.getTree()).boxed().toList();
+        assertEquals(expected, tree);
+    }
+
+    @Test
+    void shouldQuery() {
+        long[] input = {10, 5, 15, 3, 7};
+
+        SegmentTree segmentTree = new SegmentTree(input);
+
+        assertNotNull(segmentTree);
+        assertNotNull(segmentTree.getTree());
+
+        long query = segmentTree.query(2, 4);
+        assertEquals(18, query);
+    }
+
 }
