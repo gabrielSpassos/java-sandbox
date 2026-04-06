a907a9e4b44d36ee20792a430227cb20bc7a97e5
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-04-06 13:45:49 -0300

    test: add dll tests

--- queue-poc/src/test/java/com/gabrielspassos/dll/DoubleLinkedListTest.java
+++ queue-poc/src/test/java/com/gabrielspassos/dll/DoubleLinkedListTest.java
@@ -6,6 +6,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleLinkedListTest {
 
-    
+    @Test
+    void shouldAddAtEnd() {
+        IDoubleLinkedList<Integer> doubleLinkedList = new DoubleLinkedList<>();
 
+        assertTrue(doubleLinkedList.insertAtEnd(1));
+        assertTrue(doubleLinkedList.insertAtEnd(2));
+        assertTrue(doubleLinkedList.insertAtEnd(3));
+    }
+
+    @Test
+    void shouldRemoveFromBeginning() {
+        IDoubleLinkedList<Integer> doubleLinkedList = new DoubleLinkedList<>();
+
+        doubleLinkedList.insertAtEnd(4);
+        doubleLinkedList.insertAtEnd(5);
+        doubleLinkedList.insertAtEnd(6);
+
+        assertEquals(4, doubleLinkedList.removeFromBeginning());
+        assertEquals(5, doubleLinkedList.removeFromBeginning());
+        assertEquals(6, doubleLinkedList.removeFromBeginning());
+    }
+
+    @Test
+    void shouldReturnNullWithEmptyListToRemoveFromBeginning() {
+        IDoubleLinkedList<Integer> doubleLinkedList = new DoubleLinkedList<>();
+
+        assertNull(doubleLinkedList.removeFromBeginning());
+    }
+    
 }
