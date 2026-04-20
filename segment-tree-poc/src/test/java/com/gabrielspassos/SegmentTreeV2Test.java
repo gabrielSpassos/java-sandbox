1f357a77bde951c227e1501653972735ca4f3707
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-04-20 14:11:43 -0300

    feat: update segment tree update method

--- segment-tree-poc/src/test/java/com/gabrielspassos/SegmentTreeV2Test.java
+++ segment-tree-poc/src/test/java/com/gabrielspassos/SegmentTreeV2Test.java
@@ -41,4 +41,29 @@ class SegmentTreeV2Test {
         assertEquals(14, segmentTreeV2.query(4, 6));
     }
 
+    @Test
+    void shouldUpdateSegmentTree() {
+        int[] input = {5, 8, 7, 2, 10, 2, 2};
+        int[] updatedArray = {5, 8, 7, 6, 10, 2, 2};
+        List<Integer> expected = List.of(40, 26, 14, 13, 13, 12, 2, 5, 8, 7, 6, 10, 2);
+
+        SegmentTreeV2 segmentTreeV2 = new SegmentTreeV2(input);
+        segmentTreeV2.update(3, 6);
+
+        int[] array = segmentTreeV2.getArray();
+        assertNotNull(array);
+        assertEquals(toList(updatedArray), toList(array));
+
+        int[] tree = segmentTreeV2.getTree();
+        assertNotNull(tree);
+        List<Integer> actual = Arrays.stream(tree)
+                .limit(13)
+                .boxed()
+                .toList();
+        assertEquals(expected, actual);
+    }
+
+    private List<Integer> toList(int[] array) {
+        return Arrays.stream(array).boxed().toList();
+    }
 }
