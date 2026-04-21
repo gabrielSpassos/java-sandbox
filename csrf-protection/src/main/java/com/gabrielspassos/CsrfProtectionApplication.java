2daf0722914d0c407d5c389d541ba68acfc4f3b5
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-04-21 17:03:31 -0300

    feat: add csrf poc

---
+++ csrf-protection/src/main/java/com/gabrielspassos/CsrfProtectionApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielspassos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrfProtectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsrfProtectionApplication.class, args);
+	}
+
+}
