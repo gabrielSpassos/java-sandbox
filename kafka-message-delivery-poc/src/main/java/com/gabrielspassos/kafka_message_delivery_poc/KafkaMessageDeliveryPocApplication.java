2b28ce992834f69f0f0c06a5b6b461c736031e37
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-04-22 18:24:40 -0300

    feat: add kafka message delivery guarantees poc

---
+++ kafka-message-delivery-poc/src/main/java/com/gabrielspassos/kafka_message_delivery_poc/KafkaMessageDeliveryPocApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielspassos.kafka_message_delivery_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMessageDeliveryPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMessageDeliveryPocApplication.class, args);
+	}
+
+}
---
+++ kafka-message-delivery-poc/src/test/java/com/gabrielspassos/kafka_message_delivery_poc/KafkaMessageDeliveryPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielspassos.kafka_message_delivery_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMessageDeliveryPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
