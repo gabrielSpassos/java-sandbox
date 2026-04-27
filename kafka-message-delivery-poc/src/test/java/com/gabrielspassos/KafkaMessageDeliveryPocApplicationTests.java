bc624bddc13c2a7a89dc09621d52f7b85913a660
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-04-27 15:06:31 -0300

    fix: fix kafaka poc

--- kafka-message-delivery-poc/src/test/java/com/gabrielspassos/KafkaMessageDeliveryPocApplicationTests.java
+++ kafka-message-delivery-poc/src/test/java/com/gabrielspassos/KafkaMessageDeliveryPocApplicationTests.java
@@ -2,9 +2,11 @@ package com.gabrielspassos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class KafkaMessageDeliveryPocApplicationTests {
+@TestPropertySource(properties = "app.mode=AT_LEAST_ONCE")
+class KafkaMessageDeliveryPocApplicationTests extends BaseIntegrationTest {
 
 	@Test
 	void contextLoads() {
