4064c4346577193dbfbe873a83b4b51c897090bc
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-04-28 16:21:51 -0300

    refact: refactor csrf poc

--- csrf-protection/src/test/java/com/gabrielspassos/CsrfIntegrationTest.java
+++ csrf-protection/src/test/java/com/gabrielspassos/CsrfIntegrationTest.java
@@ -1,18 +1,22 @@
 package com.gabrielspassos;
 
+import com.gabrielspassos.configs.SecurityConfig;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(SecurityConfig.class)
 public class CsrfIntegrationTest {
 
     @Autowired
@@ -28,6 +32,19 @@ public class CsrfIntegrationTest {
     }
 
     @Test
+    void shouldWorkWithValidCookieAndHeader() throws Exception {
+        var token = "test-token";
+
+        mockMvc.perform(post("/transfer")
+                        .cookie(new jakarta.servlet.http.Cookie("XSRF-TOKEN", token))
+                        .header("X-XSRF-TOKEN", token)
+                        .param("amount", "123")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Transferred: 123"));
+    }
+
+    @Test
     void shouldRejectPostWhenCsrfTokenIsMissing() throws Exception {
         mockMvc.perform(post("/transfer")
                         .param("amount", "100"))
@@ -39,6 +56,14 @@ public class CsrfIntegrationTest {
         mockMvc.perform(post("/transfer")
                         .param("amount", "100")
                         .with(csrf().useInvalidToken()))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void shouldFailWhenHeaderMissingButCookiePresent() throws Exception {
+        mockMvc.perform(post("/transfer")
+                        .cookie(new Cookie("XSRF-TOKEN", "abc"))
+                        .param("amount", "100"))
                 .andExpect(status().isForbidden());
     }
 }
