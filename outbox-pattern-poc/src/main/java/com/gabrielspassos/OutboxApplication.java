bdcd7dbc681985c5702e1cbe0dd5cc87016cb57a
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-05-05 22:50:10 -0300

    feat: add outbox pattern poc

--- outbox-pattern-poc/src/main/java/com/gabrielspassos/OutboxApplication.java
+++ outbox-pattern-poc/src/main/java/com/gabrielspassos/OutboxApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthorizationApplication {
+public class OutboxApplication {
 
     static void main(String[] args) {
-		SpringApplication.run(AuthorizationApplication.class, args);
+		SpringApplication.run(OutboxApplication.class, args);
 	}
 
 }
