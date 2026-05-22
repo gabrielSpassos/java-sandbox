2bbee45dd265dd10b6287fafb0768d76762b7b04
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-05-22 09:19:59 -0300

    feat: add integration tests

--- todo-list/src/test/java/com/gabrielspassos/controller/v1/ListControllerIntegrationTest.java
+++ todo-list/src/test/java/com/gabrielspassos/controller/v1/ListControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.gabrielspassos.controller.v1;
 
 import com.gabrielspassos.BaseIntegrationTest;
+import com.gabrielspassos.controller.v1.response.ListResponse;
 import com.gabrielspassos.controller.v1.response.UserResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.UUID;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -91,6 +93,54 @@ class ListControllerIntegrationTest extends BaseIntegrationTest {
                                 "name":"it-test-fail-to-create-list-with-invalid-userId"
                             }
                         """))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("invalid id"))
+                .andExpect(jsonPath("$.code").value("INVALID_ID"));
+    }
+
+    @Test
+    void shouldFindListByUserId() throws Exception {
+        String userId = createUser("it-test-find-list-by-userId");
+        String path = String.format("/v1/users/%s/lists", userId);
+
+        MvcResult createResult = mockMvc.perform(post(path)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "name":"it-test-find-list-by-userId"
+                            }
+                        """))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        String responseBody = createResult.getResponse().getContentAsString();
+        ListResponse response = objectMapper.readValue(responseBody, ListResponse.class);
+
+        mockMvc.perform(get(path).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(response.id()))
+                .andExpect(jsonPath("$.userId").value(userId))
+                .andExpect(jsonPath("$.name").value(response.name()))
+                .andExpect(jsonPath("$.createdAt").value(response.createdAt()));
+    }
+
+    @Test
+    void shouldNotFindListByUserId() throws Exception {
+        String userId = UUID.randomUUID().toString();
+        String path = String.format("/v1/users/%s/lists", userId);
+
+        mockMvc.perform(get(path).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("List not found"))
+                .andExpect(jsonPath("$.code").value("LIST_NOT_FOUND"));
+    }
+
+    @Test
+    void shouldFailToFindListByInvalidUserId() throws Exception {
+        String userId = "invalidUserId";
+        String path = String.format("/v1/users/%s/lists", userId);
+
+        mockMvc.perform(get(path).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value("invalid id"))
                 .andExpect(jsonPath("$.code").value("INVALID_ID"));
