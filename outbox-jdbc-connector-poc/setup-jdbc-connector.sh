3cb4e75bd66d306a0dd8098a5595e1a21b69125f
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-06-03 23:26:26 -0300

    feat: update jdbc kafka connector poc

--- outbox-jdbc-connector-poc/setup-jdbc-connector.sh
+++ outbox-jdbc-connector-poc/setup-jdbc-connector.sh
@@ -20,13 +20,29 @@ echo "======================================"
 docker restart $CONNECT_CONTAINER
 
 echo "======================================"
-echo "Waiting for Kafka Connect to start"
+echo "Waiting for Kafka Connect REST API"
 echo "======================================"
 
 until curl -s $CONNECT_URL/connectors > /dev/null; do
   echo "Waiting for Kafka Connect..."
   sleep 5
 done
+
+echo "======================================"
+echo "Waiting for JDBC Plugin"
+echo "======================================"
+
+until curl -s $CONNECT_URL/connector-plugins | grep -q "JdbcSourceConnector"; do
+  echo "Waiting for JDBC plugin..."
+  sleep 5
+done
+
+echo "======================================"
+echo "Deleting old connector if exists"
+echo "======================================"
+
+curl -s -X DELETE \
+  $CONNECT_URL/connectors/outbox-source || true
 
 echo "======================================"
 echo "Registering JDBC Source Connector"
