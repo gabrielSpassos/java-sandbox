6b0cc9354188ef25f973cf4959e3ce439e202e30
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-06-04 00:04:53 -0300

    feat: start toxiproxy poc

--- toxiproxy-poc/src/test/java/com/gabrielspassos/ToxiproxyPocApplicationTests.java
+++ toxiproxy-poc/src/test/java/com/gabrielspassos/ToxiproxyPocApplicationTests.java
@@ -5,8 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
-public class ToxiproxyPocApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ToxiproxyPocApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ toxiproxy-poc/src/test/java/com/gabrielspassos/controller/v1/UserControllerIntegrationTest.java
@@ -0,0 +1,96 @@
+package com.gabrielspassos.controller.v1;
+
+import com.gabrielspassos.TestcontainersConfiguration;
+import com.gabrielspassos.controller.v1.response.UserResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void shouldCreateUser() throws Exception {
+        mockMvc.perform(post("/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "name":"it-test-user"
+                            }
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isString())
+                .andExpect(jsonPath("$.name").value("it-test-user"))
+                .andExpect(jsonPath("$.createdAt").isString());
+    }
+
+    @Test
+    void shouldFailToCreateUserWithoutName() throws Exception {
+        mockMvc.perform(post("/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "name":""
+                            }
+                        """))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Field: name invalid. Message: user must have name"))
+                .andExpect(jsonPath("$.code").value("INVALID_REQUEST"));
+    }
+
+    @Test
+    void shouldFailToCreateUserWithNullName() throws Exception {
+        mockMvc.perform(post("/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "name":null
+                            }
+                        """))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Field: name invalid. Message: user must have name"))
+                .andExpect(jsonPath("$.code").value("INVALID_REQUEST"));
+    }
+
+    @Test
+    void shouldFindUserByName() throws Exception {
+        MvcResult createResult = mockMvc.perform(post("/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                    {
+                                        "name":"it-test-user-find-by-name"
+                                    }
+                                """))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        String responseBody = createResult.getResponse().getContentAsString();
+        UserResponse response = objectMapper.readValue(responseBody, UserResponse.class);
+
+        mockMvc.perform(get("/v1/users")
+                        .queryParam("name", "it-test-user-find-by-name")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(response.id()))
+                .andExpect(jsonPath("$.name").value(response.name()))
+                .andExpect(jsonPath("$.createdAt").value(response.createdAt()));
+    }
+}
