ff05067f81d2d3ea2abff83c40f1a35a412767cc
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-06-26 09:59:10 -0300

    update toxiproxy poc

--- toxiproxy-poc/src/test/java/com/gabrielspassos/controller/v1/UserControllerChaosTest.java
+++ toxiproxy-poc/src/test/java/com/gabrielspassos/controller/v1/UserControllerChaosTest.java
@@ -2,13 +2,14 @@ package com.gabrielspassos.controller.v1;
 
 import com.gabrielspassos.BaseApplicationTest;
 import eu.rekawek.toxiproxy.model.ToxicDirection;
+import eu.rekawek.toxiproxy.model.toxic.Latency;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,8 +22,9 @@ class UserControllerChaosTest extends BaseApplicationTest {
 
     @Test
     void shouldCreateUserEvenWithDbLatency() throws Exception {
-        getDbProxy().toxics().latency("db-latency", ToxicDirection.DOWNSTREAM, 5000);
+        Latency latency = getDbProxy().toxics().latency("db-latency", ToxicDirection.DOWNSTREAM, 5000);
 
+        long start = System.currentTimeMillis();
         mockMvc.perform(post("/v1/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
@@ -33,6 +35,55 @@ class UserControllerChaosTest extends BaseApplicationTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").isString())
                 .andExpect(jsonPath("$.name").value("chaos-test-user-with-latency"))
+                .andExpect(jsonPath("$.createdAt").isString());
+        long withLatencyTimeTaken = System.currentTimeMillis() - start;
+
+        latency.remove();
+
+        long startWithoutLatency = System.currentTimeMillis();
+        mockMvc.perform(post("/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "name":"chaos-test-user-without-latency"
+                            }
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isString())
+                .andExpect(jsonPath("$.name").value("chaos-test-user-without-latency"))
+                .andExpect(jsonPath("$.createdAt").isString());
+        long withoutLatencyTimeTaken = System.currentTimeMillis() - startWithoutLatency;
+
+        assertTrue(withLatencyTimeTaken > withoutLatencyTimeTaken);
+    }
+
+    @Test
+    void shouldFailToCreateUserWithConnectionFailure() throws Exception {
+        getDbProxy().disable();
+
+        mockMvc.perform(post("/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "name":"chaos-test-user-with-connection-disabled"
+                            }
+                        """))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value("Unexpected error"))
+                .andExpect(jsonPath("$.code").value("UNEXPECTED_ERROR"));
+
+        getDbProxy().enable();
+
+        mockMvc.perform(post("/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "name":"chaos-test-user-with-connection-enabled"
+                            }
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isString())
+                .andExpect(jsonPath("$.name").value("chaos-test-user-with-connection-enabled"))
                 .andExpect(jsonPath("$.createdAt").isString());
     }
 
