0b72e2f1d59ac2acbb0f372679e8ea33f987c29a
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-06-30 22:00:24 -0300

    feat: update toxiproxy with client

--- toxiproxy-poc/src/main/java/com/gabrielspassos/client/ExchangeClient.java
+++ toxiproxy-poc/src/main/java/com/gabrielspassos/client/ExchangeClient.java
@@ -1,9 +1,14 @@
 package com.gabrielspassos.client;
 
+import com.gabrielspassos.client.response.UsdRates;
 import com.gabrielspassos.client.response.UsdResponse;
 import com.gabrielspassos.exception.NotFoundException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.math.BigDecimal;
 
 @Component
 public class ExchangeClient {
@@ -14,6 +19,8 @@ public class ExchangeClient {
         this.restClient = restClient;
     }
 
+    @Retry(name = "currency-api")
+    @CircuitBreaker(name = "currency-api", fallbackMethod = "fallbackRate")
     public UsdResponse getUsdToBrl() {
         UsdResponse response = restClient.get()
                 .uri("/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json")
@@ -25,5 +32,11 @@ public class ExchangeClient {
         }
 
         return response;
+    }
+
+    public UsdResponse fallbackRate(Throwable t) {
+        IO.println("Fallback executed: " + t.getMessage());
+
+        return new UsdResponse("2026-06-30", new UsdRates(new BigDecimal("5.18")));
     }
 }
